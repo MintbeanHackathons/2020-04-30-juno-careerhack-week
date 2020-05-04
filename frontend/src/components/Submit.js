53ed4f07fb9c91b55b490786d1982f234c1c7edf
Haley Morris <haleymor@smartt.email>
2020-05-04 10:55:48 -0400

    adding ability to add user articles

--- frontend/src/components/Submit.js
+++ frontend/src/components/Submit.js
@@ -1,24 +1,60 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+
+
 
 class Submit extends Component {
+  constructor(){
+    super();
+    this.state = {
+      title: '',
+      url: '',
+    }
+  }
+
+  onChangeTitle = (e) => {
+    this.setState({
+      title: e.target.value
+    });
+  }
+
+  onChangeUrl = (e) => {
+    this.setState ({
+      url: e.target.value
+    });
+  }
+
+  onSubmit = (e) => {
+    e.preventDefault()
+    const newArticle = {
+      title: this.state.title,
+      url: this.state.url
+    }
+    
+    axios.post('http://localhost:5000/articles/add', newArticle)
+    .then(res => console.log(res.data));
+  }
+
+  
+
+
   render() {
     return (
       <div className="submit">
         <h2>Submit Articles:</h2>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <div className="title">
             <label htmlFor="title">title: </label>
-            <input type="text" name="title" id="title" />
+            <input type="text" name="title" id="title"
+            value = {this.state.title}
+            onChange={this.onChangeTitle} />
           </div>
           <div className="url">
             <label htmlFor="url">url: </label>
-            <input type="url" name="url" id="url" />
+            <input type="url" name="url" id="url"
+            value={this.state.url}
+            onChange={this.onChangeUrl}/>
           </div>
-          {/* <p>or</p>
-          <div className="text">
-            <label htmlFor="text">text: </label>
-            <input type="textarea" name="text" id="text" />
-          </div> */}
           <div className="submitButton">
             <button>submit</button>
           </div>
