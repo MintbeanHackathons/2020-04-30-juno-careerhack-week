1cf6f22e49181565e9c5ff75f200105a60409c92
Sui.Young <suisauce.codes@gmail.com>
2020-05-04 12:34:05 -0400

    adding public folder for easy deploy

---
+++ backend/public/precache-manifest.1cc40887efadd70f2231419a920b44e3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bed331692e2426778eda524baa3e6b01",
+    "url": "/index.html"
+  },
+  {
+    "revision": "47ff85cb891e7c7ab573",
+    "url": "/static/css/main.c2d4fc5a.chunk.css"
+  },
+  {
+    "revision": "700fabd4dd0fba42390d",
+    "url": "/static/js/2.d75743da.chunk.js"
+  },
+  {
+    "revision": "4d3d17ca1d72509d468eda11f7e92a3e",
+    "url": "/static/js/2.d75743da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ff85cb891e7c7ab573",
+    "url": "/static/js/main.34c9af25.chunk.js"
+  },
+  {
+    "revision": "c3c5018b854d64cdc7f2",
+    "url": "/static/js/runtime-main.8dc0443e.js"
+  }
+]);
